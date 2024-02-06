6a7a16c3b1597b701d75366a924b415aaeff4163
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-02-06 23:37:43 +0900

    feat: RecordingWidget

--- lib/pages/diary_page.dart
+++ lib/pages/diary_page.dart
@@ -1,113 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:strecording/widgets/recording_widget.dart';
 
 class DiaryPage extends StatefulWidget {
+  const DiaryPage({super.key});
+
   @override
-  _DiaryPageState createState() => _DiaryPageState();
+  State<DiaryPage> createState() => _DiaryPageState();
 }
 
 class _DiaryPageState extends State<DiaryPage> {
+  bool isDarkened = false;
+
+  void toggleDarken() {
+    setState(() {
+      isDarkened = !isDarkened;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              children: [
-                const Expanded(
-                  child: Text(
-                    'Diary',
-                    style: TextStyle(
-                      fontFamily: 'Dongle',
-                      fontSize: 60,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.calendar_today),
-                  onPressed: () {
-                    // Handle calendar action
-                  },
-                ),
-                IconButton(
-                  icon: Icon(Icons.more_vert),
-                  onPressed: () {
-                    // Handle more action
-                  },
-                ),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                buildDiaryHeader(),
+                const SizedBox(height: 16),
+                buildDateDisplay(),
+                const SizedBox(height: 160),
+                buildDiaryTextField(),
+                const SizedBox(height: 16),
               ],
             ),
-            const SizedBox(height: 16),
-            const Expanded(
-              child: Center(
-                child: Text('2024.01.01',
-                    style: TextStyle(
-                        fontFamily: 'Dongle',
-                        fontSize: 36,
-                        fontWeight: FontWeight.normal)),
+          ),
+          if (isDarkened)
+            Positioned.fill(
+              child: GestureDetector(
+                onTap: toggleDarken,
+                child: Container(
+                  color: Colors.black.withOpacity(0.25),
+                ),
               ),
             ),
-            const SizedBox(height: 160),
-            Expanded(
-              flex: 5,
-              child: Stack(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 255, 241, 213),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: const TextField(
-                      maxLines: null,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.transparent,
-                        contentPadding:
-                            EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.transparent),
-                          borderRadius: BorderRadius.all(Radius.circular(12)),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.transparent),
-                          borderRadius: BorderRadius.all(Radius.circular(12)),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+          Positioned(
+              left: 16,
+              right: 16,
+              bottom: 10,
+              height: 80,
+              child: RecordingWidget(toggleDarken: toggleDarken)),
+        ],
+      ),
+    );
+  }
+
+  Widget buildDiaryHeader() {
+    return Row(
+      children: [
+        const Expanded(
+          child: Text(
+            'Diary',
+            style: TextStyle(
+              fontFamily: 'Dongle',
+              fontSize: 60,
+              fontWeight: FontWeight.normal,
             ),
-            const SizedBox(height: 16),
-            Container(
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 255, 252, 248),
-                borderRadius: BorderRadius.circular(12),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.25),
-                    spreadRadius: 0,
-                    blurRadius: 4,
-                    offset: Offset(0, 4), // changes position of shadow
-                  ),
-                ],
-              ),
-              width: double.infinity,
-              height: 100,
-              child: IconButton(
-                iconSize: 48,
-                icon: Icon(Icons.mic),
-                onPressed: () {
-                  // Handle recording action
-                },
-              ),
+          ),
+        ),
+        IconButton(
+          icon: Icon(Icons.calendar_today),
+          onPressed: () {
+            // Handle calendar action
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.more_vert),
+          onPressed: () {
+            // Handle more action
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget buildDateDisplay() {
+    return const Expanded(
+      child: Center(
+        child: Text('2024.01.01',
+            style: TextStyle(
+                fontFamily: 'Dongle',
+                fontSize: 36,
+                fontWeight: FontWeight.normal)),
+      ),
+    );
+  }
+
+  Widget buildDiaryTextField() {
+    return Expanded(
+      flex: 5,
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 255, 241, 213),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: const TextField(
+          maxLines: null,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.transparent,
+            contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent),
+              borderRadius: BorderRadius.all(Radius.circular(12)),
             ),
-            const SizedBox(height: 16),
-          ],
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent),
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+            ),
+          ),
         ),
       ),
     );
---
+++ lib/widgets/recording_widget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class RecordingWidget extends StatefulWidget {
+  const RecordingWidget({Key? key, required this.toggleDarken})
+      : super(key: key);
+  final VoidCallback toggleDarken;
+  @override
+  State<RecordingWidget> createState() => _RecordingWidgetState();
+}
+
+class _RecordingWidgetState extends State<RecordingWidget> {
+  bool isRecording = false;
+
+  void handlePress() {
+    setState(() {
+      isRecording = !isRecording;
+    });
+    widget.toggleDarken();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const Color primaryColor = Color(0xFF3CADAB);
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: isRecording
+              ? primaryColor
+              : const Color.fromARGB(255, 255, 252, 248),
+          // borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.25),
+              spreadRadius: 0,
+              blurRadius: 4,
+              offset: Offset(0, -4),
+            ),
+          ],
+        ),
+        child: IconButton(
+          iconSize: 24,
+          icon: isRecording
+              ? Image.asset('assets/images/recording.png')
+              : Image.asset('assets/images/record_idle.png'),
+          onPressed: () {
+            handlePress();
+          },
+        ),
+      ),
+    );
+  }
+}
