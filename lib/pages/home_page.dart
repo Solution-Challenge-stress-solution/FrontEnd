ecbb63212f80fef6802f741f53cd7673d60091d5
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-02-12 23:20:43 +0900

    feat: StressLevelWidget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:strecording/widgets/audioplayer_widget.dart';
+import 'package:strecording/widgets/stress_level.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.navigateToDiaryPage});
@@ -16,22 +17,33 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Text("Today's diary",
-                style: TextStyle(
-                  fontFamily: 'Dongle',
-                  fontWeight: FontWeight.normal,
-                  fontSize: 60,
-                )),
-            isRecorded
-                ? defaultTodaysDiary(widget.navigateToDiaryPage)
-                : Container(
-                    width: 400, height: 180, child: AudioPlayerWidget()),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text("Today's diary",
+                  style: TextStyle(
+                    fontFamily: 'Dongle',
+                    fontWeight: FontWeight.normal,
+                    fontSize: 60,
+                  )),
+              isRecorded
+                  ? defaultTodaysDiary(widget.navigateToDiaryPage)
+                  : const Column(children: <Widget>[
+                      SizedBox(
+                          width: 400, height: 180, child: AudioPlayerWidget()),
+                      Text('Daily Stress Level',
+                          style: TextStyle(
+                            fontFamily: 'Dongle',
+                            fontWeight: FontWeight.normal,
+                            fontSize: 60,
+                          )),
+                      StressLevelWidget(),
+                    ]),
+            ],
+          ),
         ),
       ),
     );
@@ -43,7 +55,7 @@ Widget defaultTodaysDiary(VoidCallback navigateToDiaryPage) {
     height: 360,
     padding: const EdgeInsets.all(24.0),
     decoration: BoxDecoration(
-      border: Border.all(color: Color.fromARGB(255, 30, 107, 125)),
+      border: Border.all(color: const Color.fromARGB(255, 30, 107, 125)),
       borderRadius: BorderRadius.circular(12),
     ),
     child: Column(
